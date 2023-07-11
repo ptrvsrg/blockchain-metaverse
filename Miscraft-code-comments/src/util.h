fcd94c09ffb60e7c8d9cf644c5373720842e4d01
ptrvsrg <s.petrov1@g.nsu.ru>
2023-07-11 09:53:09 +0700

    Delete unused functions

--- Miscraft-code-comments/src/util.h
+++ Miscraft-code-comments/src/util.h
@@ -12,20 +12,12 @@
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 double rand_double();
-void update_fps(FPS *fps);
 
-GLuint gen_buffer(GLsizei size, GLfloat *data);
-void del_buffer(GLuint buffer);
-GLfloat *malloc_faces(int components, int faces);
-GLuint gen_faces(int components, int faces, GLfloat *data);
+GLuint make_buffer(GLenum target, GLsizei size, const void *data);
 GLuint make_shader(GLenum type, const char *source);
 GLuint load_shader(GLenum type, const char *path);
 GLuint make_program(GLuint shader1, GLuint shader2);
 GLuint load_program(const char *path1, const char *path2);
 void load_png_texture(const char *file_name);
-char *tokenize(char *str, const char *delim, char **key);
-int char_width(char input);
-int string_width(const char *input);
-int wrap(const char *input, int max_width, char *output, int max_length);
 
 #endif
