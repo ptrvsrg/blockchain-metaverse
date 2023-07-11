8f3d845bfe913e18fd2a181537e45aa2701ee45a
ptrvsrg <s.petrov1@g.nsu.ru>
2023-07-11 09:53:27 +0700

    Add necessary function

--- Miscraft-code-comments/src/util.h
+++ Miscraft-code-comments/src/util.h
@@ -18,6 +18,29 @@ GLuint make_shader(GLenum type, const char *source);
 GLuint load_shader(GLenum type, const char *path);
 GLuint make_program(GLuint shader1, GLuint shader2);
 GLuint load_program(const char *path1, const char *path2);
+
+void normalize(float *x, float *y, float *z);
+void mat_identity(float *matrix);
+void mat_translate(float *matrix, float dx, float dy, float dz);
+void mat_rotate(float *matrix, float x, float y, float z, float angle);
+void mat_vec_multiply(float *vector, float *a, float *b);
+void mat_multiply(float *matrix, float *a, float *b);
+void mat_frustum(
+    float *matrix, float left, float right, float bottom,
+    float top, float znear, float zfar);
+void mat_perspective(
+    float *matrix, float fov, float aspect,
+    float near, float far);
+void mat_ortho(
+    float *matrix,
+    float left, float right, float bottom, float top, float near, float far);
+
+void make_cube(
+    float *vertex, float *normal, float *texture,
+    int left, int right, int top, int bottom, int front, int back,
+    float x, float y, float z, float n, int w);
+void make_cube_wireframe(float *vertex, float x, float y, float z, float n);
+
 void load_png_texture(const char *file_name);
 
 #endif
