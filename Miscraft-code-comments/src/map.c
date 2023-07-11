c8e8c0a47d73f9f4df76228075422909ed0322f0
ptrvsrg <s.petrov1@g.nsu.ru>
2023-07-11 10:05:03 +0700

    Mark static function

--- Miscraft-code-comments/src/map.c
+++ Miscraft-code-comments/src/map.c
@@ -2,7 +2,7 @@
 
 #include <stdlib.h>
 
-int hash_int(int key) {
+static int hash_int(int key) {
     key = ~key + (key << 15);
     key = key ^ (key >> 12);
     key = key + (key << 2);
@@ -12,34 +12,38 @@ int hash_int(int key) {
     return key;
 }
 
-int hash(int x, int y, int z) {
+static int hash(int x, int y, int z) {
     x = hash_int(x);
     y = hash_int(y);
     z = hash_int(z);
     return x ^ y ^ z;
 }
 
-void map_alloc(Map *map, int dx, int dy, int dz, int mask) {
-    map->dx = dx;
-    map->dy = dy;
-    map->dz = dz;
-    map->mask = mask;
+static void map_grow(Map *map) {
+    Map new_map;
+    new_map.mask = (map->mask << 1) | 1;
+    new_map.size = 0;
+    new_map.data = (MapEntry *)calloc(new_map.mask + 1, sizeof(MapEntry));
+    for (unsigned int index = 0; index <= map->mask; index++) {
+        MapEntry *entry = map->data + index;
+        if (!EMPTY_ENTRY(entry)) {
+            map_set(&new_map, entry->x, entry->y, entry->z, entry->w);
+        }
+    }
+    free(map->data);
+    map->mask = new_map.mask;
+    map->size = new_map.size;
+    map->data = new_map.data;
+}
+
+void map_alloc(Map *map) {
+    map->mask = 0xfff;
     map->size = 0;
     map->data = (MapEntry *)calloc(map->mask + 1, sizeof(MapEntry));
 }
 
 void map_free(Map *map) {
     free(map->data);
-}
-
-void map_copy(Map *dst, Map *src) {
-    dst->dx = src->dx;
-    dst->dy = src->dy;
-    dst->dz = src->dz;
-    dst->mask = src->mask;
-    dst->size = src->size;
-    dst->data = (MapEntry *)calloc(dst->mask + 1, sizeof(MapEntry));
-    memcpy(dst->data, src->data, (dst->mask + 1) * sizeof(MapEntry));
 }
 
 int map_set(Map *map, int x, int y, int z, int w) {
@@ -95,21 +99,3 @@ int map_get(Map *map, int x, int y, int z) {
     }
     return 0;
 }
-
-void map_grow(Map *map) {
-    Map new_map;
-    new_map.dx = map->dx;
-    new_map.dy = map->dy;
-    new_map.dz = map->dz;
-    new_map.mask = (map->mask << 1) | 1;
-    new_map.size = 0;
-    new_map.data = (MapEntry *)calloc(new_map.mask + 1, sizeof(MapEntry));
-    MAP_FOR_EACH(map, ex, ey, ez, ew) {
-        map_set(&new_map, ex, ey, ez, ew);
-    } END_MAP_FOR_EACH;
-    free(map->data);
-    map->mask = new_map.mask;
-    map->size = new_map.size;
-    map->data = new_map.data;
-}
-
