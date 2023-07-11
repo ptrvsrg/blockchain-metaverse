044ef8b9a0639060c2f1a48d3ded20abd5c5c5a1
ptrvsrg <s.petrov1@g.nsu.ru>
2023-07-11 14:14:30 +0700

    load flipped png

--- MyCraft/src/util.c
+++ MyCraft/src/util.c
@@ -736,34 +736,15 @@ void make_cube_wireframe(float *vertex, float x, float y, float z, float n) {
     *(v++) = z + n;
 }
 
-void flip_image_vertical(
-    unsigned char *data, unsigned int width, unsigned int height)
-{
-    unsigned int size = width * height * 4;
-    unsigned int stride = sizeof(char) * width * 4;
-    unsigned char *new_data = malloc(sizeof(unsigned char) * size);
-    for (unsigned int i = 0; i < height; i++) {
-        unsigned int j = height - i - 1;
-        memcpy(new_data + j * stride, data + i * stride, stride);
-    }
-    memcpy(data, new_data, size);
-    free(new_data);
-}
-
 void load_png_texture(const char *file_name) {
-    unsigned int error;
-    unsigned char *data;
-    unsigned int width, height;
-    error = lodepng_decode32_file(&data, &width, &height, file_name);
+    unsigned error;
+    unsigned char *image;
+    unsigned width, height;
+    error = lodepng_decode32_file(&image, &width, &height, file_name);
     if (error) {
-        fprintf(stderr, "load_png_texture %s failed, error %u: %s\n",
-                file_name, error, lodepng_error_text(error));
-        exit(1);
+        fprintf(stderr, "error %u: %s\n", error, lodepng_error_text(error));
     }
-    flip_image_vertical(data, width, height);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
-        GL_UNSIGNED_BYTE, data);
-    // Free the data because lodepng_decode32_file() allocated it,
-    // and we copied the data over to OpenGL.
-    free(data);
+                 GL_UNSIGNED_BYTE, image);
+    free(image);
 }
