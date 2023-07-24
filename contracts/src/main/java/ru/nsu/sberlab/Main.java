3fd2b3937baa4312ce6f8216a9006fbe2b9d097c
Vladislav Stepanyuk <v.stepanyuk@g.nsu.ru>
2023-07-24 06:13:27 +0000

    add recording hashes to config

--- contracts/src/main/java/ru/nsu/sberlab/Main.java
+++ contracts/src/main/java/ru/nsu/sberlab/Main.java
@@ -1,13 +1,23 @@
 package ru.nsu.sberlab;
 
 import io.neow3j.types.ContractParameter;
+import io.neow3j.types.Hash160;
 import io.neow3j.wallet.Account;
 import ru.nsu.sberlab.contracts.CompilationDeploying;
+import ru.nsu.sberlab.contracts.MapContractAlreadyExist;
+import ru.nsu.sberlab.contracts.StateContractAlreadyExist;
+
+import java.io.*;
 
 public class Main {
 
     private static final String ownerWifEnvVar = "OWNER_WIF";
     private static final String nodeURLEnvVar = "NODE_URL";
+
+    private static final String CONFIG_FILE_PATH = "start-menu/src/main/resources/config/connection.properties";
+
+    private static final String MAP_HASH_CONF_NAME = "connection.hash160_map";
+    private static final String STATE_HASH_CONF_NAME = "connection.hash160_state"
 
     private static final String nameEnvVar = "NAME";
 
@@ -19,21 +29,39 @@ public class Main {
 
         CompilationDeploying compilationDeploying = new CompilationDeploying(httpURL, account);
 
-        try {
+        Hash160 mapContractHash;
+        Hash160 stateContractHash;
 
-            System.out.printf("map contract Hash: %s%n", compilationDeploying.deployMapContract(ContractParameter.hash160(account.getScriptHash()), name).toString());
+        try {
+            mapContractHash = compilationDeploying.deployMapContract(ContractParameter.hash160(account.getScriptHash()), name);
+        } catch (MapContractAlreadyExist e) {
+            mapContractHash = e.getMapContractExistHash();
         } catch (Throwable e) {
             e.printStackTrace();
+            return;
         }
 
         try {
-
-            System.out.printf("state contract Hash: %s%n", compilationDeploying.deployStateContract(ContractParameter.hash160(account.getScriptHash()), name).toString());
-
+            stateContractHash = compilationDeploying.deployStateContract(ContractParameter.hash160(account.getScriptHash()), name);
+        } catch (StateContractAlreadyExist e) {
+            stateContractHash = e.getStateContractExistHash();
         } catch (Throwable e) {
+            e.printStackTrace();
+            return;
+        }
+
+
+
+        try (FileWriter fileWriter = new FileWriter(CONFIG_FILE_PATH)) {
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.printf("%s=%s\n", MAP_HASH_CONF_NAME, mapContractHash);
+            printWriter.printf("%s=%s\n",STATE_HASH_CONF_NAME, stateContractHash);
+        }catch (Exception e) {
             e.printStackTrace();
         }
 
 
     }
+
+
 }
